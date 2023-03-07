c9e3975a771db8cd1924981e6804af6a32c22af2
Carina Balasa <carina_marina07@yahoo.com>
2023-03-07 18:18:21 +0200

    first draft

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello world")
+import pygame
+pygame.init
+black=(0,0,0)
+size=(700,700)
+screen=pygame.display.set_mode(size)
+pygame.display.set_caption("My game")
+clock=pygame.time.Clock()
+
+class Player:
+    def __init__ (self, name, age)
+        
+
+done=False
+
+while done==False:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+
+
+    screen.fill(black)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
+    
+
