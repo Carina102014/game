8a6791eecda359f5b6ce3e16d209582e24138b72
Carina Balasa <carina_marina07@yahoo.com>
2023-03-17 12:26:49 +0200

    added extra fetures

--- gui.py
+++ gui.py
@@ -4,19 +4,20 @@ class GameGui():
     def __init__(self,version):
        self.color=(0,0,0) 
        self.size=(700,700)
-       clock=pygame.time.Clock()
+       self.clock=pygame.time.Clock()
+       self.version = version
 
 
     def build(self):
         self.screen=pygame.display.set_mode(self.size)
-        pygame.display.set_caption("My game")
-        self.screen.fill(0,0,0)
-        pygame.display.flip()
+        pygame.display.set_caption(f"My game {self.version}")
+        self.set_clock(200)
+        self.screen.fill((0,0,0))
+        
 
     
     def set_clock(self,new_clock):
-        self.clock=new_clock
- 
+        self.clock.tick(new_clock) 
 
  
 
--- tool.py
+++ tool.py
@@ -1,9 +1,25 @@
+import pygame
+
 class Tool():
     def __init__(self, color):
-        self.shape = ""
+        self.shape = "rect"
         self.color = color
+        self.size = (30, 30)
+        self.position = (60, 60)
+
+
+    def build(self, screen):
+        if self.shape == "rect":    
+            pygame.draw.rect(screen, self.color, pygame.Rect(self.position, self.size))
+            pygame.display.flip()
 
     def change_color(self, new_color):
         self.color = new_color
 
-    print(f"{__name__}")
+    def draw_tool(self, screen, x, y):
+        screen.fill((0,0,0))
+        self.position = (x, y)
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.position, self.size))
+        pygame.display.flip()
+
+
