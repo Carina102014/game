af76e2af0761fa75ea420bf72c3e50a5077f485a
Alexandre <alexurluescu23@gmail.com>
2023-03-19 11:57:26 +0200

    main: Create a RECT what can move on the screen

--- gui.py
+++ gui.py
@@ -6,14 +6,13 @@ class GameGui():
        self.size=(700,700)
        self.clock=pygame.time.Clock()
        self.version = version
-
+       
 
     def build(self):
         self.screen=pygame.display.set_mode(self.size)
         pygame.display.set_caption(f"My game {self.version}")
         self.set_clock(200)
         self.screen.fill((0,0,0))
-        
 
     
     def set_clock(self,new_clock):
--- main.py
+++ main.py
@@ -9,20 +9,49 @@ def main():
     pygame.init()
     my_screen=gui.GameGui("version 0.0.1")
     my_screen.build()
-    player_one = player.Player(name="Carina", age=20)
-    player_one.tool.build(my_screen.screen)
-    time.sleep(4)
-    player_one.tool.change_color("red")
-    player_one.tool.draw_tool(my_screen.screen, 80, 80)
-    target_1=target.Target()
-    target_1.draw(surface = my_screen.screen)
+    # player_one = player.Player(name="Carina", age=20)
+    # player_one.tool.build(my_screen.screen)
+    # time.sleep(4)
+    # player_one.tool.change_color("red")
+    # player_one.tool.draw_tool(my_screen.screen, 80, 80)
+    # time.sleep(4)
+    # target_1=target.Target()
+    # target_1.draw(surface = my_screen.screen)
+    # time.sleep(4)
 
     done=False
+
+    vel = 1
+    x = 100
+    y = 200
+
+    width = 20
+    height = 20
+
 
     while done==False:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 done=True
+        
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and x > 0:
+            x -= vel
+
+        if keys[pygame.K_RIGHT] and x < 700 - width:
+            x += vel
+        
+        if keys[pygame.K_UP] and y > 0:
+            y -= vel
+        
+        if keys[pygame.K_DOWN] and y < 700 - height:
+            y += vel
+
+        my_screen.screen.fill((0,0,0))
+
+
+        pygame.draw.rect(my_screen.screen, "blue", (x,y, width, height))
         pygame.display.update()
 
     pygame.quit()
