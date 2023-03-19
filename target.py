4877c05b2fc4f165de0cdb2d271a894dfdf32e22
Alexandre <alexurluescu23@gmail.com>
2023-03-19 11:59:19 +0200

    target:Add new properties when create a new target

--- target.py
+++ target.py
@@ -3,21 +3,27 @@ import random
 
 
 class Target:
-    def __init__(self):
-        self.shape = "circle"
-        self.color = "green"
+    def __init__(self, shape, color, x, y):
+        self.shape = shape
+        self.color = color
         self.position = [random.randint(0, 600), random.randint(0, 500)]
         self.radius = 10
+        self.x = x
+        self.y = y
     
     def draw(self, surface):
-        surface.fill((0,0,0))
+        # surface.fill((0,0,0))
 
         #surface = the screen
         # self.color = what color will have the shape
         # (122, 250) = coordonates -> x = 122 and y=250
         # 20 = radius of the circle
         # 0 = fill with color all the circle
-        pygame.draw.circle(surface, self.color, (122,250), 20, 0)
-        pygame.display.flip()
+        if(self.shape == "circle"):
+            pygame.draw.circle(surface, self.color, (self.x,self.y), 20, 0)
+
+        if(self.shape == "rect"):
+            pygame.draw.rect(surface, self.color, (self.x, self.y, 50, 50))
+        # pygame.display.flip()
     
      
