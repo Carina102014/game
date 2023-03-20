e1a03c4cc0ff6d21902e473729210b3633a7ee0c
Alexandre <alexurluescu23@gmail.com>
2023-03-20 21:01:32 +0200

    tool: Create new properties in Tool Class()

--- tool.py
+++ tool.py
@@ -1,17 +1,21 @@
 import pygame
 
 class Tool():
-    def __init__(self, color):
+    def __init__(self, color, x, y, width, height):
         self.shape = "rect"
         self.color = color
         self.size = (30, 30)
         self.position = (60, 60)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
 
     def build(self, screen):
         if self.shape == "rect":    
-            pygame.draw.rect(screen, self.color, pygame.Rect(self.position, self.size))
-            pygame.display.flip()
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+            # pygame.display.flip()
 
     def change_color(self, new_color):
         self.color = new_color
@@ -20,6 +24,6 @@ class Tool():
         screen.fill((0,0,0))
         self.position = (x, y)
         pygame.draw.rect(screen, self.color, pygame.Rect(self.position, self.size))
-        pygame.display.flip()
+        # pygame.display.flip()
 
 
